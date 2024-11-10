a85e0b1eb644e8bc7d26b83c6ecd2d07c61361d4
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2024-11-10 18:52:00 +0000

    Add card guesses

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -92,6 +93,34 @@ func distributeCards(deck []Card) ([][]string, [][]string, [][]string, [][]strin
 	return player1, player2, player3, player4
 }
 
+// countCardsOfRank counts how many cards of a given rank a player has
+func countCardsOfRank(cards [][]string, rank string) int {
+	count := 0
+	for _, card := range cards {
+		fmt.Println("card: ", card[0])
+		if strings.Contains(card[0], rank) {
+			// if card[0][4:6] == rank { // Check the rank (first part of the card's rank in string)
+			count++
+		}
+	}
+	return count
+}
+
+// moveCards moves cards from one player's hand to another (here, from another player to player 1)
+func moveCards(from *[][]string, to *[][]string, rank string) {
+	// Find and remove the cards of the given rank from the 'from' player
+	var cardsToMove [][]string
+	for i, card := range *from {
+		if card[0][4:6] == rank {
+			cardsToMove = append(cardsToMove, card)
+			*from = append((*from)[:i], (*from)[i+1:]...) // Remove card
+			break
+		}
+	}
+	// Add the found cards to the 'to' player's hand
+	*to = append(*to, cardsToMove...)
+}
+
 func main() {
 	// Generate and shuffle the deck
 	deck := generateDeck()
@@ -104,4 +133,64 @@ func main() {
 	printHand("Player 2", player2) // AI bot 1
 	printHand("Player 3", player3) // AI bot 2
 	printHand("Player 4", player4) // AI bot 3
+
+	// Game loop for player1 to interact with other players
+	for {
+		// Ask player1 which AI player they want to talk to
+		var playerChoice string
+		fmt.Print("Which player do you want to talk to? (player2, player3, player4): ")
+		fmt.Scanln(&playerChoice)
+		playerChoice = strings.ToLower(playerChoice)
+
+		// Choose the correct player hand based on playerChoice
+		var selectedPlayer [][]string
+		switch playerChoice {
+		case "player2":
+			selectedPlayer = player2
+		case "player3":
+			selectedPlayer = player3
+		case "player4":
+			selectedPlayer = player4
+		default:
+			fmt.Println("Invalid player choice. Please try again.")
+			continue
+		}
+
+		// Prompt player1 for a card rank
+		var rankChoice string
+		fmt.Print("Enter the card rank you want to ask about (A, 2, 3, ..., K): ")
+		fmt.Scanln(&rankChoice)
+		rankChoice = strings.ToUpper(rankChoice)
+
+		// Prompt player1 to guess how many cards the selected player has of that rank
+		var guess int
+		fmt.Printf("How many %s cards does %s have? ", rankChoice, playerChoice)
+		fmt.Scanln(&guess)
+
+		// Count how many cards the selected player has of the chosen rank
+		actualCount := countCardsOfRank(selectedPlayer, rankChoice)
+
+		// Check if the guess was correct
+		if guess == actualCount {
+			fmt.Printf("Correct! Player 1 takes all %d cards of rank %s from %s.\n", actualCount, rankChoice, playerChoice)
+			// Move the cards to player 1's hand
+			moveCards(&selectedPlayer, &player1, rankChoice)
+		} else {
+			fmt.Printf("Wrong! %s has %d cards of rank %s, not %d.\n", playerChoice, actualCount, rankChoice, guess)
+		}
+
+		// Ask if player 1 wants to continue
+		var continueGame string
+		fmt.Print("Do you want to continue? (yes/no): ")
+		fmt.Scanln(&continueGame)
+		if strings.ToLower(continueGame) != "yes" {
+			break
+		}
+
+		// Print updated hands
+		printHand("Player 1", player1)
+		printHand("Player 2", player2)
+		printHand("Player 3", player3)
+		printHand("Player 4", player4)
+	}
 }
